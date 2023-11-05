8141e6bd5574539dfac1308dcede367a2d469767
ostefanski <oliver.stefanskii@gmail.com>
2023-11-05 17:20:32 +0100

    added Nearby places search, changed map style, made it functional when the place of deafult marker is being changed on the map

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -11,6 +11,12 @@ const Map: React.FC<MapProps> = ({ chosenCity, selectedLocation }) => {
 	const mapRef = useRef<HTMLElement | null>(null);
 	const mapInstance = useRef<google.maps.Map | null>(null);
 	const markerInstance = useRef<google.maps.Marker | null>(null);
+	const redCircleinstance = useRef<google.maps.Circle | null>(null);
+	const greenCircleinstance = useRef<google.maps.Circle | null>(null);
+	const nearbyMarkersInstance = useRef<google.maps.Marker[] | null>(null);
+	const previousSelectedLocation = useRef<google.maps.LatLng | undefined>();
+	const previousChosenCity = useRef<string>();
+	const mapPreviousClickedLocation = useRef<google.maps.LatLng | undefined>();
 
 	const getCityPosition = (city: string): google.maps.LatLngLiteral => {
 		switch (city) {
@@ -25,28 +31,134 @@ const Map: React.FC<MapProps> = ({ chosenCity, selectedLocation }) => {
 		}
 	};
 
+	const createCircles = async (defaultLocation) => {
+		if (mapInstance.current === null) {
+			console.error('Map is not initialized.');
+			return;
+		}
+
+		if (defaultLocation) {
+			//red Circle
+			redCircleinstance.current = new google.maps.Circle({
+				strokeColor: '#FF5252',
+				strokeOpacity: 0.8,
+				strokeWeight: 2,
+				fillColor: '#FF5252',
+				fillOpacity: 0.05,
+				zIndex: 2,
+				clickable: false,
+				draggable: false,
+				editable: false,
+				visible: true,
+				map: mapInstance.current,
+				center: defaultLocation,
+				radius: 2200,
+			});
+
+			// green Circle
+			greenCircleinstance.current = new google.maps.Circle({
+				strokeColor: '#8BC34A',
+				strokeOpacity: 0.5,
+				strokeWeight: 2,
+				fillColor: '#8BC34A',
+				fillOpacity: 0.05,
+				zIndex: 3,
+				clickable: false,
+				draggable: false,
+				editable: false,
+				visible: true,
+				map: mapInstance.current,
+				center: defaultLocation,
+				radius: 1100,
+			});
+		}
+	};
+
+	const findNearbyPlaces = async (defaultLocation) => {
+		const types = ['bank', 'hospital', 'university', 'bus_station', 'police', 'doctor', 'school', 'store'];
+
+		if (mapInstance.current === null) {
+			console.error('Map is not initialized.');
+			return;
+		}
+
+		// Create a PlacesService instance
+		const service = new google.maps.places.PlacesService(mapInstance.current);
+
+		// Define the request parameters
+		const request = {
+			location: defaultLocation,
+			radius: 1100, // 1.1 km
+			types: types,
+		};
+
+		const nearbyMarkers: google.maps.Marker[] = [];
+
+		// Send the nearby search request
+		service.nearbySearch(request, (results, status) => {
+			if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+				// Loop through the results and add markers to the map
+				results.forEach((place) => {
+					if (place.geometry && place.geometry.location) {
+						const nearbyMarker = new google.maps.Marker({
+							position: place.geometry.location,
+							map: mapInstance.current,
+							title: place.name,
+							visible: true,
+						});
+
+						nearbyMarkers.push(nearbyMarker);
+					}
+				});
+			}
+		});
+		nearbyMarkersInstance.current = nearbyMarkers;
+	};
+
 	const initMapWithMarker = async () => {
 		const position = getCityPosition(chosenCity);
+
+		const blueMarkerIcon = {
+			url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+		};
 
 		// Setup the map
 		if (mapRef.current !== null) {
 			mapInstance.current = await initMap(mapRef.current, position);
-			// markerInstance.current = await initMarker(position, mapInstance.current);
-			// initMarker(position, mapInstance.current);
+
 			markerInstance.current = new google.maps.Marker({
 				position: position,
 				map: mapInstance.current,
 				visible: false,
+				icon: blueMarkerIcon,
 			});
 
 			markerInstance.current.addListener('click', () => {
+				// disable marker, circles, and other nearby markers when the deafult marker is clicked again
 				markerInstance.current?.setVisible(false);
+				greenCircleinstance.current?.setVisible(false);
+				redCircleinstance.current?.setVisible(false);
+				nearbyMarkersInstance.current?.forEach((marker) => {
+					marker.setVisible(false);
+				});
 			});
 
 			mapInstance.current.addListener('click', (event) => {
 				const clickedLocation = event.latLng;
+				// if clickedLocation is different than previous one, disable previous circles and markers
+				if (clickedLocation !== mapPreviousClickedLocation.current) {
+					mapPreviousClickedLocation.current = clickedLocation;
+					greenCircleinstance.current?.setVisible(false);
+					redCircleinstance.current?.setVisible(false);
+					nearbyMarkersInstance.current?.forEach((marker) => {
+						marker.setVisible(false);
+					});
+				}
+
 				markerInstance.current?.setPosition(clickedLocation);
 				markerInstance.current?.setVisible(true);
+				findNearbyPlaces(clickedLocation);
+				createCircles(clickedLocation);
 			});
 		}
 	};
@@ -56,11 +168,22 @@ const Map: React.FC<MapProps> = ({ chosenCity, selectedLocation }) => {
 		if (mapInstance.current === null) {
 			initMapWithMarker();
 		} else {
+			// If previous location is different than current one, disable previouse's location circles and markers
+			if (selectedLocation !== previousSelectedLocation.current) {
+				previousSelectedLocation.current = selectedLocation;
+				greenCircleinstance.current?.setVisible(false);
+				redCircleinstance.current?.setVisible(false);
+				nearbyMarkersInstance.current?.forEach((marker) => {
+					marker.setVisible(false);
+				});
+			}
 			if (selectedLocation) {
 				mapInstance.current?.panTo(selectedLocation);
 				if (markerInstance.current) {
 					markerInstance.current.setPosition(selectedLocation);
 					markerInstance.current?.setVisible(true);
+					findNearbyPlaces(selectedLocation);
+					createCircles(selectedLocation);
 				}
 			}
 		}
@@ -70,9 +193,20 @@ const Map: React.FC<MapProps> = ({ chosenCity, selectedLocation }) => {
 		if (mapInstance.current) {
 			const newCityPosition = getCityPosition(chosenCity);
 			mapInstance.current.panTo(newCityPosition);
+			// If previous chosenCity is different than current one, disable previouse's chosenCity circles and markers
+			if (chosenCity !== previousChosenCity.current) {
+				previousChosenCity.current = chosenCity;
+				greenCircleinstance.current?.setVisible(false);
+				redCircleinstance.current?.setVisible(false);
+				nearbyMarkersInstance.current?.forEach((marker) => {
+					marker.setVisible(false);
+				});
+			}
 			if (markerInstance.current) {
 				markerInstance.current.setPosition(newCityPosition);
-				markerInstance.current.setVisible(false);
+				markerInstance.current.setVisible(true);
+				findNearbyPlaces(newCityPosition);
+				createCircles(newCityPosition);
 			}
 		}
 	}, [chosenCity]);
@@ -81,3 +215,14 @@ const Map: React.FC<MapProps> = ({ chosenCity, selectedLocation }) => {
 };
 
 export default Map;
+
+//TODO:
+// 3. dodaj funkcjonalność że do znacznika który jest najbliżej startowego znacznika wyświetlana jest odrazu droga.
+// 4. dodaj możliwość kliknięcia na pozostałe znaczniki i wyświetlenia do nich drogi.
+// 5. informacja o dokładnej odległości między zaznaczonymi znacznikami i czasie podróży.
+// 6. zmodyfikuj działanie znaczników i obszarów, jeżeli w obszarze zielonym nie ma żadnych znaczników, to powinny się
+// pojawić pozostałe znaczniki z obszaru czerwonego o ile takowe istnieja.
+// 7. dodać dodatkowo clusterowanie znaczników, żeby rozdzielały się w grupy w zależności od ich ilości w danym miejscu.
+// 8. poprawić mape bo jak się oddala na maxa to nie ma ograniczenia i mapa się spłaszcza i zostaja szare ramki do okoła.
+// 9. poprawić i potweakować z wyszukiwaniem interesującyh nas miejsc w okół.
+// 10. stworzyć możliwość wybierania kategorii miejsc które interesują użytkownika i zaadaptować logikę do tego.
--- src/utils/GoogleApi.ts
+++ src/utils/GoogleApi.ts
@@ -1,4 +1,5 @@
 import { Loader } from '@googlemaps/js-api-loader';
+import { mapStyle } from 'src/utils/mapStyle';
 
 const apiKey: string = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
@@ -16,39 +17,13 @@ export const initMap = async (map: HTMLElement, position: google.maps.LatLngLite
 	const mapOptions = {
 		center: position, // Set initial center
 		zoom: 12,
-		mapId: 'MY_REACT_MAPID',
 		disableDefaultUI: true,
 		clickableIcons: false,
+		styles: mapStyle,
 	};
 
 	return new mapInitialization.Map(map, mapOptions);
 };
-
-// export const initMarker = async (position: google.maps.LatLngLiteral, map: google.maps.Map) => {
-// 	const marker = await GoogleApiLoader.importLibrary('marker');
-
-// 	// Marker options
-// 	const markerOptions = {
-// 		position: position,
-// 		map: map,
-// 		visible: false,
-// 	};
-
-// 	// Setup the marker
-// 	const googleMarker = new marker.Marker(markerOptions);
-
-// 	googleMarker.addListener('click', () => {
-// 		googleMarker.setVisible(false);
-// 	});
-
-// 	map.addListener('click', (event) => {
-// 		const clickedLocation = event.latLng;
-// 		googleMarker.setPosition(clickedLocation);
-// 		googleMarker.setVisible(true);
-// 	});
-
-// 	return googleMarker;
-// };
 
 // Function to initialize the Autocomplete service
 export const initAutocompleteService = async (inputElement: HTMLInputElement) => {
